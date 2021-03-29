c1794cb4005daaaa71eb840a094816daffcadfdd
Андрей <azazaandrey407@gmail.com>
2021-03-29 18:55:23 +0300

    Cleaned theme footer

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
-</div> <!-- wrapper -->
+</div>
 <div id="footer">
-	<a href="<?php bloginfo('rss2_url'); ?>">Публикации (RSS)</a> и <a href="<?php bloginfo('comments_rss2_url'); ?>">Комментарии (RSS)</a>.<br />
-	<a href="http://wp-templates.ru/">Темы WordPress</a>.<br />
-	<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	<a href="<?php bloginfo('rss2_url'); ?>">Публикации (RSS)</a> и <a href="<?php bloginfo('comments_rss2_url'); ?>">Комментарии (RSS)</a>
 	<?php wp_footer(); ?>
 </div>
-</div> <!-- page -->
-</body>
-</html>
+</div>
