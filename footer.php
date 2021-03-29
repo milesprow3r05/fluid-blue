c24cfd6c0703f613f5fff28374eb169a201365fa
Андрей <azazaandrey407@gmail.com>
2021-03-29 20:21:05 +0300

    Added MySQL number processed queries and cleaned footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
-</div>
+</div> 
 <div id="footer">
-	<a href="<?php bloginfo('rss2_url'); ?>">Публикации (RSS)</a> и <a href="<?php bloginfo('comments_rss2_url'); ?>">Комментарии (RSS)</a>
+	<a href="<?php bloginfo('rss2_url'); ?>">Публикации (RSS)</a> и <a href="<?php bloginfo('comments_rss2_url'); ?>">Комментарии (RSS)</a>.<br />
+	<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds.
 	<?php wp_footer(); ?>
 </div>
 </div>
