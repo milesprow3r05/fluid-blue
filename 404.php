9ad9dd2cfbe146442d794139b33bfcdf0a2c473e
Андрей <azazaandrey407@gmail.com>
2021-03-29 20:06:27 +0300

    Deleted header

--- 404.php
+++ 404.php
@@ -1,7 +1,5 @@
-<?php get_header(); ?>
-
 <div id="content">
-<h2 class="center"><?php _e('Ошибка') ?> 404 - <?php _e('Не найдено') ?></h2>
+<h2 class="center"><?php _e('Ошибка') ?> 404 - <?php _e('Не найдена страница/пост!') ?></h2>
 </div>
 	
 <?php get_footer(); ?>
